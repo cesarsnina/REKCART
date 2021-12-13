061e4e0833006710d7b55407314de5a50365b46c
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-13 23:32:12 +0000

    installed & set up cors, began work on workout.js and its parent containers

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require("cors")
 const express = require('express');
 const path = require('path');
 
@@ -8,6 +9,7 @@ const seed = require('./server/seed/seed.js');
 
 seed();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
