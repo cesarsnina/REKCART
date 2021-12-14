69fd9c0a9a2d799f61c947874cbb7245465eed1a
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-14 23:14:48 +0000

    worked on handleDelete

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 
 const Workout = ({workout}) => {
     return (
@@ -8,17 +7,16 @@ const Workout = ({workout}) => {
             {/* NEED TO ADD LOADING STATE??? */}
             {console.log("Inside workout:", workout)}
             {workout ? (
-                <Link to={`${workout.id}`}>
+                <>
                     <h1>Date: {workout.date}</h1>
                     <h1>Calories: {workout.calories}</h1>
                     <h1>Type: {workout.type}</h1>
                     <h1>Time: {workout.time}</h1>
-                </Link>
+                </>
             ) : (
                 <h1>"This workout no longer exists..."</h1>
             )}
             <br/>
-            {/* { MAY NOT BE NECESSARY TO ADD CONDITIONAL RENDERING BASED ON PAGE } */}
         </div>
     );
 }
