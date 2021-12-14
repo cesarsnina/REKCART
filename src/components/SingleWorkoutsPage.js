1a70fb55569d31340eb8e2fb2fdd5379d18f1bf3
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-14 17:08:15 +0000

    changed AllWorkoutsPage and SingleWorkoutsPage path

--- src/components/SingleWorkoutsPage.js
+++ src/components/SingleWorkoutsPage.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Workout from './Workout';
 
 const Singleworkoutspage = () => {
+    const url = "http://localhost:3001/users"
+    // User will get to this page via a link in UserPage, AllWorkoutsPage, or SingleWorkoutPage
+    // The url will hold the Wid and Uid
+    // Use the Wid & Uid from url to fetch it inside useEffect
+    // Pass it down as prop to workout component
+
+    // set workout state to pass it to Workout Component
+    const [workout, setWorkout] = useState(null)
+
+    useEffect(() => {
+        handleFetch()
+    }, [])
+    
+    const handleFetch = () => {
+        console.log(`${url}/${window.location.pathname.split("/")[2]}`)
+         fetch(`${url}/${window.location.pathname.split("/")[2]}`) // RETRIEVES WID FROM URL (I know this is bad practice)
+            .then(res => res.json())
+            .then((data) => {
+                console.log("inside fetch:", data)
+                setWorkout(data.workouts[1]) // -------- HARD CODING WORKOUT --------
+            })
+    }
+
+    // NEED handleUpdate method
+    const handleUpdate = (e) => {
+        
+    }
     // NEED handleDelete method
+    const handleDelete = (e) => {
+        // retrieve workout we're deleting
+        // set new state
+    }
+
     // NEED WORKOUT FORM COMPONENT TO EDIT
 
     return (
         <div>
+            {console.log("hello")}
             <Workout />
+            <h3>WORKOUT FORM WOULD GO HERE</h3>
         </div>
     );
 }
