6fb20697fdf60b96aae07be4e63c1c97539b5759
Danny Reina <dannyreina@google.com>
2021-12-15 17:01:01 +0000

    feat: enable sign up feature for user

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { UserContext } from "./UserContext.js"
+import { useNavigate } from "react-router-dom"
+
 
 const Signup = () => {
+  const {globalUser, setGlobalUser} = useContext(UserContext)
+  const [values, setValues] = useState('')
+  const navigate = useNavigate()
+
+  const HandleSubmit = (event) => {
+    event.preventDefault()
+    const newAccountData = {
+      name: values.name,
+      email: values.email,
+      password: values.password,
+      image: values.image
+    }
+
+    const newUser = fetch(`http://localhost:3001/api/users/create-account`, {
+      method: 'POST',
+      headers : { 
+        'Content-Type': 'application/json',
+       },
+      body: JSON.stringify(newAccountData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        return data
+      })
+
+      setGlobalUser(newUser)
+      navigate('/userpage')
+  }
+
+  const handleChange = async (event) => {
+    const value = event.target.value;
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
     return (
-        <div>
-            <h1>Signup</h1>
-        </div>
+      <form onSubmit={HandleSubmit}>
+        <input type="text" name="name" placeHolder="Your name" value={values.name} onChange={handleChange} required/>
+        <input type="email" name="email" placeHolder="Your Email" value={values.email} onChange={handleChange} required/>
+        <input type="password" name="password" placeHolder="Your Password" value={values.password} onChange={handleChange} required/>
+        <input type="text" name="image" placeHolder="Image URL" value={values.image} onChange={handleChange} required/>
+        <button type="submit">Create Account</button>
+      </form>
     );
 }
 
