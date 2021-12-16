c30bc64c116ad693a384720fb44b832b4bf80eec
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-16 20:57:56 +0000

    successfully put together everyone's code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import AllWorkoutsPage from './components/AllWorkoutsPage';
+import AllWorkoutsPage2 from './components/AllWorkoutsPage2';
 import Login from './components/Login';
-import SingleWorkoutsPage from './components/SingleWorkoutsPage2';
+import SingleWorkoutsPage2 from './components/SingleWorkoutsPage2';
 import Signup from './components/Signup';
 import UserPage from './components/UserPage';
 import {UserContext, FilterQueryContext} from './components/UserContext.js'
@@ -24,8 +24,8 @@ const App = () => {
       ) : (
         <Routes>
             <Route path='/' element={<UserPage />} />
-            <Route path='/users/:id/workouts' element={<AllWorkoutsPage />} />
-            <Route path='/users/:id/workouts/:wid' element={<SingleWorkoutsPage />} />
+            <Route path='/users/:id/workouts' element={<AllWorkoutsPage2 />} />
+            <Route path='/users/:id/workouts/:wid' element={<SingleWorkoutsPage2 />} />
         </Routes>
       )}
       </FilterQueryContext.Provider>
